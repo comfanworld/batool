7a09619f5b8157737b662e5d076be72aa4a95df2
comfan <comfanchina@gmail.com>
2016-11-16 20:44:12 +0800

    no arguments prompt.

--- security/messagedigest/md5fline.sh
+++ security/messagedigest/md5fline.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#  Arguments ： readfile 
+#  readfile   a file ready read and calculate hash.
+if $# eq 0 ; then
+    echo "Missed Arguments ： readfile"
+    echo "readfile   a file ready read and calculate hash"
+    exit 1
+fi
+
 for line in $(cat $1)
 do
     echo -n $line|md5sum|awk '{print $1}'
